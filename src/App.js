f0b7a9ca67896e0793689fc81d3e5223fe7b1a5d
Michał Stępień <michal.stepien36@gmail.com>
2018-11-30 13:42:08 +0100

    Grid component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 import Toolbar from './components/Toolbar/Toolbar'
-import MainNews from './components/main/Main-news'
+import MainNews from './components/Main/Main-news'
+import Grid from './components/Grid/Grid'
 
 import './App.css'
 
@@ -11,7 +12,7 @@ class App extends Component {
       <div className="App">
         <Toolbar/>
         <main className='main'>
-          <MainNews/>
+          <Grid/>
         </main>
       </div>
     );
