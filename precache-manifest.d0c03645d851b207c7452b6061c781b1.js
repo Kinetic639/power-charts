c873fcffdf7acfd44d2199b1253190dabf5a2640
Michał Stępień <michal.stepien36@gmail.com>
2018-11-30 18:18:52 +0100

    Updates

---
+++ precache-manifest.d0c03645d851b207c7452b6061c781b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33345dd92df6eea57230",
+    "url": "/power-charts/static/js/runtime~main.33345dd9.js"
+  },
+  {
+    "revision": "d99d9edf282417338404",
+    "url": "/power-charts/static/js/main.d99d9edf.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/power-charts/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "d99d9edf282417338404",
+    "url": "/power-charts/static/css/main.94112d20.chunk.css"
+  },
+  {
+    "revision": "c53ae8e92696b647abc5f5e54f3b3fb9",
+    "url": "/power-charts/index.html"
+  }
+];
